e228639b4b600491287549b171142352e7756984
jgespinosa10 <jgespinosa@uc.cl>
2019-06-30 11:32:01 -0400

    probando cosas simples

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, json
 from pymongo import MongoClient
 from json import dumps
-import os
-import atexit
-import subprocess
+# import os
+# import atexit
+# import subprocess
 
-USER_KEYS = ['name', 'last_name', 'occupation', 'follows', 'age']
-MESS_KEYS = ['id', 'message', 'sender', 'receptant', 'lat', 'long', 'date']
-texto = ["incluye", "opcion", "no"]
+# USER_KEYS = ['name', 'last_name', 'occupation', 'follows', 'age']
+# MESS_KEYS = ['id', 'message', 'sender', 'receptant', 'lat', 'long', 'date']
+# texto = ["incluye", "opcion", "no"]
 
 
-# Levantamos el servidor de mongo. Esto no es necesario, puede abrir
-# una terminal y correr mongod. El DEVNULL hace que no vemos el output
-mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
-# Nos aseguramos que cuando el programa termine, mongod no quede corriendo
-uri = "mongodb://grupo39:grupo39@146.155.13.149/grupo39?authSource=admin"
-atexit.register(mongod.kill)
+# # Levantamos el servidor de mongo. Esto no es necesario, puede abrir
+# # una terminal y correr mongod. El DEVNULL hace que no vemos el output
+# mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
+# # Nos aseguramos que cuando el programa termine, mongod no quede corriendo
+# uri = "mongodb://grupo39:grupo39@146.155.13.149/grupo39?authSource=admin"
+# atexit.register(mongod.kill)
 
-# El cliente se levanta en localhost:5432
-client = MongoClient(uri)
-# Utilizamos la base de datos 'api'
-db = client["grupo39"]
-# # Seleccionamos la colección de usuarios
-messages = db.messages
-users = db.usuarios
+# # El cliente se levanta en localhost:5432
+# client = MongoClient(uri)
+# # Utilizamos la base de datos 'api'
+# db = client["grupo39"]
+# # # Seleccionamos la colección de usuarios
+# messages = db.messages
+# users = db.usuarios
 
-# for i in list(messages.find({}, {"_id":0})):
-#     print(str(i).encode("utf-8"))
+# # for i in list(messages.find({}, {"_id":0})):
+# #     print(str(i).encode("utf-8"))
 
 # Iniciamos la aplicación de flask
 app = Flask(__name__)
@@ -43,81 +43,81 @@ def home():
     return str
 
 
-@app.route("/messages/<int:id>")
-def get_messages(id):
-    mensaje = list(messages.find({"id": id}, {"_id": 0}))
-    return json.jsonify(mensaje)
+# @app.route("/messages/<int:id>")
+# def get_messages(id):
+#     mensaje = list(messages.find({"id": id}, {"_id": 0}))
+#     return json.jsonify(mensaje)
 
 
-@app.route("/messages/<int:uid1>/<int:uid2>")
-def get_user_mensaje(uid1, uid2):
-    mensajes1 = list(messages.find({"$and": [{"sender": uid1}, {"receptant": uid2}]}, {"_id": 0}))
-    mensajes2 = list(messages.find({"$and": [{"sender": uid2}, {"receptant": uid1}]}, {"_id": 0}))
-    return json.jsonify(mensajes1 + mensajes2)
+# @app.route("/messages/<int:uid1>/<int:uid2>")
+# def get_user_mensaje(uid1, uid2):
+#     mensajes1 = list(messages.find({"$and": [{"sender": uid1}, {"receptant": uid2}]}, {"_id": 0}))
+#     mensajes2 = list(messages.find({"$and": [{"sender": uid2}, {"receptant": uid1}]}, {"_id": 0}))
+#     return json.jsonify(mensajes1 + mensajes2)
 
 
-@app.route("/user/<int:uid>")
-def get_user(uid):
-    usuario = list(users.find({"usuid": uid}, {"_id": 0}))
-    emitidos = list(messages.find({"sender": uid}, {"_id": 0}))
-    return json.jsonify(usuario + emitidos)
+# @app.route("/user/<int:uid>")
+# def get_user(uid):
+#     usuario = list(users.find({"usuid": uid}, {"_id": 0}))
+#     emitidos = list(messages.find({"sender": uid}, {"_id": 0}))
+#     return json.jsonify(usuario + emitidos)
 
 
-@app.route("/messages")
-def get_message():
-    message = list(messages.find({}, {"_id": 0}))
-    return json.jsonify(message)
+# @app.route("/messages")
+# def get_message():
+#     message = list(messages.find({}, {"_id": 0}))
+#     return json.jsonify(message)
 
 
-@app.route("/messages", methods=['POST'])
-def create_message():
-    data = {key: request.json[key] for key in MESS_KEYS}
-    count = messages.count_documents({})
-    data["id"] = count + 1
-    result = messages.insert_one(data)
-    if result:
-        message = "1 mensaje creado"
-        success = True
-    else:
-        message = "No se pudo crear el mensaje"
-        success = False
-    return json.jsonify({'success': success, 'message': message})
+# @app.route("/messages", methods=['POST'])
+# def create_message():
+#     data = {key: request.json[key] for key in MESS_KEYS}
+#     count = messages.count_documents({})
+#     data["id"] = count + 1
+#     result = messages.insert_one(data)
+#     if result:
+#         message = "1 mensaje creado"
+#         success = True
+#     else:
+#         message = "No se pudo crear el mensaje"
+#         success = False
+#     return json.jsonify({'success': success, 'message': message})
 
 
-@app.route('/messages', methods=['DELETE'])
-def delete_message():
-    _id = {'id': request.json['id']}
-    messages.delete_one({"id": _id["id"]})
-    message = 'Mensaje con id={} ha sido eliminado.'.format(_id)
-    return json.jsonify({'result': 'success', 'message': message})
+# @app.route('/messages', methods=['DELETE'])
+# def delete_message():
+#     _id = {'id': request.json['id']}
+#     messages.delete_one({"id": _id["id"]})
+#     message = 'Mensaje con id={} ha sido eliminado.'.format(_id)
+#     return json.jsonify({'result': 'success', 'message': message})
 
 
-@app.route('/search')
-def buscar():
-    text = {key: request.json[key] for key in texto}
+# @app.route('/search')
+# def buscar():
+#     text = {key: request.json[key] for key in texto}
 
-    # Esto borra todos los usuarios con el id dentro de la lista4
-    incluir = text["incluye"].split("/")
+#     # Esto borra todos los usuarios con el id dentro de la lista4
+#     incluir = text["incluye"].split("/")
 
-    search = []
+#     search = []
 
-    for i in incluir:
-        search.append('\"' + i + '\"')
+#     for i in incluir:
+#         search.append('\"' + i + '\"')
 
-    opcion = text["opcion"].split("/")
+#     opcion = text["opcion"].split("/")
 
-    for i in opcion:
-        search.append(i)
+#     for i in opcion:
+#         search.append(i)
 
-    no = text["no"].split("/")
-    for i in no:
-        search.append('-' + i)
+#     no = text["no"].split("/")
+#     for i in no:
+#         search.append('-' + i)
 
-    result = dumps(messages.find({"$text": {"$search": ' '.join(search)}}))
+#     result = dumps(messages.find({"$text": {"$search": ' '.join(search)}}))
 
-    # Retorno el texto plano de un json
-    return result
+#     # Retorno el texto plano de un json
+#     return result
 
 
-if os.name == '__main__':
-    app.run()
+# if os.name == '__main__':
+#     app.run()
