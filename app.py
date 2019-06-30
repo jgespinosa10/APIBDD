fef8acb0d840e58e1cd594218f6cb15aac048608
jgespinosa10 <jgespinosa@uc.cl>
2019-06-30 11:41:16 -0400

    3

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ MESS_KEYS = ['id', 'message', 'sender', 'receptant', 'lat', 'long', 'date']
 texto = ["incluye", "opcion", "no"]
 
 
-# # Levantamos el servidor de mongo. Esto no es necesario, puede abrir
-# # una terminal y correr mongod. El DEVNULL hace que no vemos el output
-# mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
-# # Nos aseguramos que cuando el programa termine, mongod no quede corriendo
-# uri = "mongodb://grupo39:grupo39@146.155.13.149/grupo39?authSource=admin"
-# atexit.register(mongod.kill)
+# Levantamos el servidor de mongo. Esto no es necesario, puede abrir
+# una terminal y correr mongod. El DEVNULL hace que no vemos el output
+mongod = subprocess.Popen('mongod', stdout=subprocess.DEVNULL)
+# Nos aseguramos que cuando el programa termine, mongod no quede corriendo
+uri = "mongodb://grupo39:grupo39@146.155.13.149/grupo39?authSource=admin"
+atexit.register(mongod.kill)
 
 # # El cliente se levanta en localhost:5432
 # client = MongoClient(uri)
